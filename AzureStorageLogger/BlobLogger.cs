815e914a18c79b8029f8cfbec182eace0ae5ba19
Nikkitha Ramesh <nikkitha.nikki96@gmail.com>
2019-08-01 23:11:55 +0200

    dll

---
+++ AzureStorageLogger/BlobLogger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Azure;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Azure.Storage.RetryPolicies;
+using Microsoft.Azure.Storage.Shared.Protocol;
+using System.Threading;
+using NLog;
+
+
+namespace AzureStorageLogger
+{
+    public class BlobLogger
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+        public static void ConnectToBlob()
+        {
+            CloudStorageAccount storageAccount;
+
+            try
+            {
+                storageAccount = CloudStorageAccount.Parse("connectionString");
+            }
+
+            
+            catch (StorageException e)
+            {
+                Logger.Error(e, "could not establish connection with blob storage");
+                throw;
+            }
+
+            finally
+            {
+                LogManager.Shutdown();
+            }
+
+        }
+    }
+}
